550c3318a731e802da5bcdd6c7e34b979d7f4b48
Kandriws <kewin_live@hotmail.es>
2024-09-01 17:56:30 +0200

    Rename MessagePatter

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -34,7 +34,7 @@ export class ProductsController {
   }
 
   // @Delete(':id')
-  @MessagePattern({ cmd: 'delete_product' })
+  @MessagePattern({ cmd: 'remove_product' })
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
